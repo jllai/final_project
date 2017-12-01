c30b8296697f43eb424d39cc465021b8c2b1c100
Jamie Lai <jllai@umich.edu>
2017-12-01 00:21:54 -0500

    Done with finding names

--- finalproject.py
+++ finalproject.py
@@ -7,6 +7,7 @@ import sqlite3
 import gspread
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from collections import defaultdict
 
 #authenticates the two sheets
 scope = ['https://spreadsheets.google.com/feeds']
@@ -30,7 +31,6 @@ def get_new_user_info():
 	# Define your function get_user_tweets here:
 	if os.path.isfile('dnc.json'):
 		print("using cache")
-		# global CACHE_DICTION
 		return CACHE_DICTION
 	else:
 		print("fetching")
@@ -38,12 +38,13 @@ def get_new_user_info():
 			#opens the sheet
 			name_lst = client1.open("File to de-duplicate").sheet1
 			#pulls everything from the sheet and puts it into a list of dictionaries
-			name_check = name_lst.get_all_records()
+			name_check = name_lst.col_values(8)
 			CACHE_DICTION = name_check
 			dumped_json_cache = json.dumps(CACHE_DICTION)
 			fw = open(CACHE_NEW,"w")
 			fw.write(dumped_json_cache)
 			fw.close() # Close the open file
+			name_lst.close()
 			return CACHE_DICTION
 		except:
 			print("Wasn't in cache and wasn't valid search either")
@@ -65,14 +66,13 @@ def get_all_user_info():
 	# Define your function get_user_tweets here:
 	if os.path.isfile('all_users.json'):
 		print("using cache")
-		# global CACHE_DICTION
 		return CACHE_DICTION
 	else:
 		print("fetching")
 		try:
 			#opens the sheet
 			all_lst = client2.open("Existing Data").sheet1
-			all_names = all_lst.get_all_records()
+			all_names = all_lst.col_values(9)
 			CACHE_DICTION = all_names
 			dumped_json_cache = json.dumps(CACHE_DICTION)
 			fw = open(CACHE_NEW,"w")
@@ -85,18 +85,14 @@ def get_all_user_info():
 
 all_users = get_all_user_info()
 
-i = 0
-for ppl_info in all_users:
-	if ppl_info['Primary Email Address'] in new_user[i]:
-		i += 1
-		continue
+for i in range(len(all_users)):
+	all_users[i] = all_users[i].lower()
 
-
-
-
-
-
-
+name_lst = client1.open("File to de-duplicate").sheet1
+for ppl in new_user[1:]:
+	ppl = ppl.lower()
+	if ppl in all_users:
+		name_lst.delete_row(new_user.index(ppl) + 1)
 
 
 
